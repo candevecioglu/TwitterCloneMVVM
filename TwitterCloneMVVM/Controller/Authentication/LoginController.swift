f42756ea6032c1b4566db073aaaea70ba7e57dec
M. Can Devecioğlu <can.devecioglu@gmail.com>
2022-08-28 00:13:07 +0300

    no message

--- TwitterCloneMVVM/Controller/Authentication/LoginController.swift
+++ TwitterCloneMVVM/Controller/Authentication/LoginController.swift
@@ -18,6 +18,30 @@ class LoginController: UIViewController {
         return iv
     }()
     
+    private lazy var emailContainerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .red
+        view.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        let iv = UIImageView()
+        iv.image = #imageLiteral(resourceName: "ic_mail_outline_white_2x-1")
+        view.addSubview(iv)
+        iv.anchor(left: view.leftAnchor, bottom: view.bottomAnchor, paddingLeft: 8, paddingBottom: 8)
+        iv.setDimensions(width: 24, height: 24)
+        return view
+    }()
+    
+    private lazy var passwordContainerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .purple
+        view.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        let iv = UIImageView()
+        iv.image = #imageLiteral(resourceName: "ic_lock_outline_white_2x")
+        view.addSubview(iv)
+        iv.anchor(left: view.leftAnchor, bottom: view.bottomAnchor, paddingLeft: 8, paddingBottom: 8)
+        iv.setDimensions(width: 24, height: 24)
+        return view
+    }()
+    
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
@@ -38,6 +62,15 @@ class LoginController: UIViewController {
         view.addSubview(logoImageView)
         logoImageView.centerX(inView: view, topAnchor: view.safeAreaLayoutGuide.topAnchor)
         logoImageView.setDimensions(width: 150, height: 150)
+        
+        
+        let stack = UIStackView(arrangedSubviews: [emailContainerView, passwordContainerView])
+        stack.axis = .vertical
+        stack.spacing = 8
+        
+        view.addSubview(stack)
+        
+        stack.anchor(top: logoImageView.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor)
     }
 
 }
