23f563aa96b51cc362b365c4a2aabce1394d41e4
M. Can Devecioğlu <can.devecioglu@gmail.com>
2022-08-29 00:00:15 +0300

    no message

--- TwitterCloneMVVM/Controller/Authentication/LoginController.swift
+++ TwitterCloneMVVM/Controller/Authentication/LoginController.swift
@@ -19,27 +19,26 @@ class LoginController: UIViewController {
     }()
     
     private lazy var emailContainerView: UIView = {
-        let view = UIView()
-        view.backgroundColor = .red
-        view.heightAnchor.constraint(equalToConstant: 50).isActive = true
-        let iv = UIImageView()
-        iv.image = #imageLiteral(resourceName: "ic_mail_outline_white_2x-1")
-        view.addSubview(iv)
-        iv.anchor(left: view.leftAnchor, bottom: view.bottomAnchor, paddingLeft: 8, paddingBottom: 8)
-        iv.setDimensions(width: 24, height: 24)
+        let image = #imageLiteral(resourceName: "ic_mail_outline_white_2x-1")
+        let view = Utilities().inputContainerView(withImage: image, textField: emailTextField)
         return view
     }()
     
     private lazy var passwordContainerView: UIView = {
-        let view = UIView()
-        view.backgroundColor = .purple
-        view.heightAnchor.constraint(equalToConstant: 50).isActive = true
-        let iv = UIImageView()
-        iv.image = #imageLiteral(resourceName: "ic_lock_outline_white_2x")
-        view.addSubview(iv)
-        iv.anchor(left: view.leftAnchor, bottom: view.bottomAnchor, paddingLeft: 8, paddingBottom: 8)
-        iv.setDimensions(width: 24, height: 24)
+        let image = #imageLiteral(resourceName: "ic_lock_outline_white_2x")
+        let view = Utilities().inputContainerView(withImage: image, textField: passwordTextField)
         return view
+    }()
+    
+    private let emailTextField: UITextField = {
+        let tf = Utilities().textField(withPlaceholder: "Email")
+        return tf
+    }()
+    
+    private let passwordTextField: UITextField = {
+        let tf = Utilities().textField(withPlaceholder: "Password")
+        tf.isSecureTextEntry = true
+        return tf
     }()
     
     // MARK: - Lifecycle
@@ -70,7 +69,7 @@ class LoginController: UIViewController {
         
         view.addSubview(stack)
         
-        stack.anchor(top: logoImageView.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor)
+        stack.anchor(top: logoImageView.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingLeft: 16, paddingRight: 16)
     }
 
 }
---
+++ TwitterCloneMVVM/Utils/Utilities.swift
@@ -0,0 +1,43 @@
+//
+//  Utilities.swift
+//  TwitterCloneMVVM
+//
+//  Created by M. Can Devecioğlu on 28.08.2022.
+//
+
+import UIKit
+
+class Utilities {
+    
+    func inputContainerView (withImage image: UIImage, textField: UITextField) -> UIView {
+        
+        let view = UIView()
+        let iv = UIImageView()
+        view.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
+        iv.image = image
+        view.addSubview(iv)
+        iv.anchor(left: view.leftAnchor, bottom: view.bottomAnchor, paddingLeft: 8, paddingBottom: 8)
+        iv.setDimensions(width: 24, height: 24)
+        
+        view.addSubview(textField)
+        textField.anchor(left: iv.rightAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, paddingLeft: 8, paddingBottom: 8)
+        
+        let dividerView = UIView()
+        dividerView.backgroundColor = .white
+        view.addSubview(dividerView)
+        dividerView.anchor(left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, paddingLeft: 8, height: 0.75)
+        
+        return view
+        
+    }
+    
+    func textField(withPlaceholder placeholder: String) -> UITextField {
+        let tf = UITextField()
+        tf.textColor = .white
+        tf.font = UIFont.systemFont(ofSize: 16)
+        tf.attributedPlaceholder = NSAttributedString(string: placeholder, attributes: [NSAttributedString.Key.foregroundColor: UIColor.white])
+        return tf
+    }
+    
+}
