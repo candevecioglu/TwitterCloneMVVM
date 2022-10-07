fc05b129066b2801a0c27d9cc267e496b3e4ebe6
M. Can Devecioğlu <can.devecioglu@gmail.com>
2022-10-07 23:48:58 +0300

    no message

--- TwitterCloneMVVM/View/Profile/EditProfileCell.swift
+++ TwitterCloneMVVM/View/Profile/EditProfileCell.swift
@@ -11,6 +11,30 @@ class EditProfileCell : UITableViewCell {
     
     //MARK: - Properties
     
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14)
+        return label
+    }()
+    
+    lazy var infoTextField: UITextField = {
+        let tf = UITextField()
+        tf.borderStyle = .none
+        tf.font = UIFont.systemFont(ofSize: 14)
+        tf.textAlignment = .left
+        tf.textColor = .twitterBlue
+        tf.addTarget(self, action: #selector(handleUpdateUserInfo), for: .editingDidEnd)
+        return tf
+    }()
+    
+    let bioTextView: CaptionTextView = {
+        let tv = CaptionTextView()
+        tv.font = UIFont.systemFont(ofSize: 14)
+        tv.textColor = .twitterBlue
+        tv.placeholderLabel.text = "Bio"
+        return tv
+    }()
+    
     //MARK: - Lifecycle
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
