0f2ed2cc981b7d3d819093717c24b083c9b48856
M. Can Devecioğlu <can.devecioglu@gmail.com>
2022-10-23 21:46:48 +0300

    no message

--- TwitterCloneMVVM/View/UserCell.swift
+++ TwitterCloneMVVM/View/UserCell.swift
@@ -14,17 +14,17 @@ class UserCell: UITableViewCell {
     }
     
     private lazy var profileImageView: UIImageView = {
-       let iv = UIImageView()
-        iv.contentMode = .scaleAspectFit
-        iv.clipsToBounds = true
+       let iv                  = UIImageView()
+        iv.contentMode         = .scaleAspectFit
+        iv.clipsToBounds       = true
         iv.setDimensions(width: 40, height: 40)
-        iv.layer.cornerRadius = 40 / 2
-        iv.backgroundColor = .twitterBlue
+        iv.layer.cornerRadius  = 40 / 2
+        iv.backgroundColor     = .twitterBlue
         return iv
     }()
     
     private let usernameLabel: UILabel = {
-       let label = UILabel()
+       let label   = UILabel()
         label.font = UIFont.boldSystemFont(ofSize: 14)
         return label
     }()
